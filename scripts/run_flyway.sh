7ade8dd9c0bfe3891746b59e9e44be34029df5f1
Yury_Popov <yuryi.popov@quantori.com>
2023-03-24 14:04:04 +0400

    first

---
+++ scripts/run_flyway.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#The script for running migrations locally
+BASE_PATH="/home/uppy/code/labs/flyway_lab"
+ACCOUNT="ACCOUNT_NAME"
+FLYWAY_USER="DB_USER"
+FLYWAY_PASSWORD="DB_PASSWORD"
+FLYWAY_DEFAULT_SCHEMA="DEFAULT_SCHEMA"
+DB_NAME="DB_NAME"
+WAREHOUSE_NAME="WAREHOUSE_NAME"
+ROLE="USER_ROLE"
+FLYWAY_URL="jdbc:snowflake://${ACCOUNT}.snowflakecomputing.com/?db=${DB_NAME}&warehouse=${WAREHOUSE_NAME}&role=${ROLE}"
+
+docker run \
+  --rm \
+  -v "${BASE_PATH}/db/migrations":/flyway/sql \
+  -e FLYWAY_USER=${FLYWAY_USER} \
+  -e FLYWAY_PASSWORD=${FLYWAY_PASSWORD} \
+  -e FLYWAY_DEFAULT_SCHEMA=${FLYWAY_DEFAULT_SCHEMA} \
+  -e FLYWAY_URL=${FLYWAY_URL} \
+  -e FLYWAY_PLACEHOLDER_REPLACEMENT="true" \
+  -e FLYWAY_PLACEHOLDERS_WAREHOUSE=${WAREHOUSE_NAME} \
+  -e FLYWAY_PLACEHOLDERS_DB_NAME=${DB_NAME} \
+  -e FLYWAY_VALIDATE_MIGRATION_NAMING=true \
+  flyway/flyway:9.10.1 $1
